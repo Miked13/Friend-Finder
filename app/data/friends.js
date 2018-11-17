18da86d32157d68897410d2b9041cd00b83dc069
Scorpio <diallomikey13@yahoo.fr>
2018-11-17 09:24:03 -0500

    update

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+// DATA
+// This is where we store data of the friends looking for their match
+var friends = [
+    {
+        name: "Eric",
+        photo: "https://www.maxpixel.net/static/photo/2x/Portrait-Male-Young-Person-Smile-Profile-Face-1726471.jpg",
+        scores: [5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        name: "Esmeralda Botticelli",
+        photo: "https://www.maxpixel.net/static/photo/2x/Young-Girl-Portrait-Girl-Lady-Facial-Woman-Eye-139353.jpg",
+        scores: [2,1,3,3,4,1,5,2,3,2]
+    },
+    {
+        name: "Maria Spinozza",
+        photo: "https://www.maxpixel.net/static/photo/2x/Natural-Profile-Family-Face-Girl-Photo-1722498.jpg",
+        scores: [1,3,5,3,5,5,3,4,4,2]
+    },
+    {
+        name: "Angelo Bronte",
+        photo: "https://www.maxpixel.net/static/photo/2x/Adult-People-Lifestyle-Portrait-Man-Recreation-3049894.jpg",
+        scores: [3,3,1,4,5,2,2,4,5,5]
+    },
+    {
+        name: "Edward Grant",
+        photo: "https://www.maxpixel.net/static/photo/2x/Male-Young-Portrait-Guy-Person-Man-Handsome-1246508.jpg",
+        scores: [2,2,3,1,4,4,5,2,5,4]
+    },
+    {
+        name: "Sandra Bella",
+        photo: "https://www.maxpixel.net/static/photo/2x/Eyes-Portrait-Face-Woman-Redhead-Scarf-Hair-1828099.jpg",
+        scores: [5,5,4,5,4,3,3,2,1,2]
+    }
+];
+
+// Exportation of the arrays
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,35 @@
+// LOAD DATA
+// This is the link connecting the routes 
+var friends = require("../data/friends");
+
+//ROUTING
+module.exports = function(app){
+    //API GET requests
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    })
+    //API POST Requests
+    // Add new friend
+	app.post("/api/friends", function(req, res) {
+        //user input of new friends added to the list
+        var addedfriendScores = req.body.scores;
+        var scoresArray = [];
+        var bestMatch = 0;
+
+        for(var i = 0; i < friends.length; i++){
+            var scoresDiff = 0;
+            for(var j = 0; i < addedfriendScores.length; i++){
+                scoresDiff += Math.abs(parseInt(friends[i].scores[j]) - parseInt(addedfriendScores[j]));
+            }
+            scoresArray.push(scoresDiff);
+        }
+        for(var i = 0;i < scoresArray.length; i++){
+            if(scoresArray[i] <= scoresArray[bestMatch]){
+                bestMatch = i;
+            }
+        }
+        var compatibleFriends = friends[bestMatch];
+        res.json(compatibleFriends);
+        friends.push(req.body);
+	});
+};
